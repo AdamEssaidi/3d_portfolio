75c56dc98fccdf982b9ed44c96fc8eae473c888b
adam essaidi <adam.essaidi01@gmail.com>
2026-01-11 13:29:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  NavBar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return <div>3D Developer Portfolio</div>;
+};
+
+export default App;
